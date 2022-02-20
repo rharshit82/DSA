a4d7d1b2077b332b4b4affb40eb130cc1a32d9b3
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-02-20 12:41:53 +0530

    Time: 197 ms (65.75%), Space: 51.7 MB (48.32%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool DFSRec(int s,vector<bool>&visited,vector<bool>&dfsVisited,vector<vector<int>>&graph,vector<bool>&present_cycle){
+        visited[s]=true;
+        dfsVisited[s]=true;
+        vector<int>data=graph[s];
+        for(auto x:data){
+            if(!visited[x]){
+                if(DFSRec(x,visited,dfsVisited,graph,present_cycle))
+                    return present_cycle[s]=true;
+            }
+            else if(visited[x]&&dfsVisited[x])
+                return present_cycle[s]=true;
+        }
+        dfsVisited[s]=false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> visited(n,false),dfsVisited(n,false);
+        vector<bool>present_cycle(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                DFSRec(i,visited,dfsVisited,graph,present_cycle);
+            }
+        }
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            if(!present_cycle[i]) res.push_back(i);
+        }
+        return res;
+    }
+};
