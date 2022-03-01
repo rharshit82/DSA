abf05a909391e6edb401f6a20dacbf1444da8a7e
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-03-01 11:58:21 +0530

    Time: 251 ms (76.63%), Space: 59.8 MB (83.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> ppi;
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<ppi> pq;
-        for(auto ele: points){
-            pq.push({ele[0]*ele[0]+ele[1]*ele[1],{ele[0],ele[1]}});
+        int n=points.size();
+        priority_queue<pair<int,pair<int,int>>> pq;
+        for(int i=0;i<n;i++){
+            int dis=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+            pq.push({dis,{points[i][0],points[i][1]}});
             if(pq.size()>k) pq.pop();
         }
-        vector<vector<int>> res;
-        while(!pq.empty()){
-            vector<int> temp;
-            temp.push_back(pq.top().second.first);
-            temp.push_back(pq.top().second.second);
-            res.push_back(temp);
+        vector<vector<int>> res(k,vector<int>(2));
+        for(int i=0;i<k;i++){
+            res[i][0]=pq.top().second.first;
+            res[i][1]=pq.top().second.second;
             pq.pop();
         }
         return res;
+        
+        
     }
 };
