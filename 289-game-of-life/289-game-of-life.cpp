c45e5dbcf39801754329d0027da34bae12d83a11
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-17 02:33:23 +0530

    Time: 1 ms (45.75%), Space: 7 MB (20.79%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    bool isSafe(int i, int j, int R, int C){
-        if(i>=0 and i<R and j>=0 and j<C) return true;
-        return false;
-    }
     void gameOfLife(vector<vector<int>>& board) {
-        int R = board.size();
-        int C = board[0].size();
+        int n=board.size();
+        int m=board[0].size();
         int dx[] = {1,-1,0,0,1,-1,1,-1};
         int dy[] = {0,0,1,-1,1,-1,-1,1};
-        for(int i=0; i<R;i++){
-            for(int j=0;j<C; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 int count=0;
                 for(int k=0; k<8; k++){
-                    if(isSafe(i+dx[k], j+ dy[k], R, C) && abs(board[i+dx[k]][j+dy[k]])==1) count++;
+                    int nr = i+dx[k];
+                    int nc = j+dy[k];
+                    if(nr<0 or nr>=n or nc<0 or nc>=m) continue;
+                    if(abs(board[nr][nc])==1) count++;
                 }
-                if(abs(board[i][j])==1 && (count!=2 && count!=3)) board[i][j]=-1;
-                if(board[i][j]==0 && count==3) board[i][j]=2;
+                if(abs(board[i][j]) and (count!=2 and count!=3)) board[i][j]=-1;
+                if(board[i][j]==0 and count==3) board[i][j]=2;
             }
         }
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(board[i][j]==-1) board[i][j]=0;
                 if(board[i][j]==2) board[i][j]=1;
             }
