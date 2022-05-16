89c6254dbee4594c1afcbc567a4ea564d244bc19
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-16 12:10:12 +0530

    Time: 7 ms (8.08%), Space: 6.9 MB (32.12%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int dir=0;
-        int top=0;
-        int left=0;
-        int down=matrix.size()-1;
-        int right=matrix[0].size()-1;
+        int n=matrix.size();
+        int m=matrix[0].size();
         vector<int> res;
-        while(top<=down and left<=right){
+        int left=0,right=m-1;
+        int top=0,down=n-1;
+        int dir=0;
+        while(top<=down and right>=left){
             if(dir==0){
-                for(int i=left;i<=right;i++){
-                    res.push_back(matrix[top][i]);
+               for(int i=left; i<=right; i++){
+                res.push_back(matrix[top][i]);
                 }
-                top++;
+                top++; 
             }
-             if(dir==1){
-                for(int i=top;i<=down;i++){
-                    res.push_back(matrix[i][right]);
+            if(dir==1){
+                for(int i=top; i<=down; i++){
+                res.push_back(matrix[i][right]);
                 }
                 right--;
             }
-             if(dir==2){
-                for(int i=right;i>=left;i--){
-                    res.push_back(matrix[down][i]);
+            if(dir==2){
+                for(int i=right; i>=left; i--){
+                res.push_back(matrix[down][i]);
                 }
                 down--;
             }
-             if(dir==3){
-                for(int i=down;i>=top;i--){
-                    res.push_back(matrix[i][left]);
+            if(dir==3){
+                for(int i=down; i>=top; i--){
+                res.push_back(matrix[i][left]);
                 }
                 left++;
             }
             dir=(dir+1)%4;
         }
+        
         return res;
     }
 };
