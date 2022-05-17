9911e9b9926806972a2c124c18eb3d85186831c6
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-17 22:07:25 +0530

    Time: 16 ms (23.93%), Space: 20.8 MB (24.59%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     string reverseWords(string s) {
         string res="";
-        string sn="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' ' and sn.size()==0)  continue;
+        int n=s.size();
+        int i=0;
+        while(i<n){
             if(s[i]==' '){
-                res=sn+' '+res;
-                sn="";
-            }else{
-                sn+=s[i];
+                i++;
             }
-        }
-        if(sn.size()>0){
-             res=sn+" "+res;
+            else {
+                string temp="";
+                while(s[i]!=' ' and i<n){
+                    temp+=s[i];
+                    i++;
+                }
+                res=temp+' '+res;
+            }
         }
         res.pop_back();
         return res;
