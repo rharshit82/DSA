67e12ccca9890425251a396d45e05e9db56c1c20
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-23 22:58:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (48.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,7 +8,12 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        return count(n,dp,nums);
+        vector<int> dp(n+1,0);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2; i<=n; i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[n];
     }
 };
