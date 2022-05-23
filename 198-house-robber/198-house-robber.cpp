749b847a1befcab3013f68f027f599d7939dedb8
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-23 23:06:15 +0530

    Time: 3 ms (50.11%), Space: 7.6 MB (76.53%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int count(int n, vector<int>&dp,vector<int>& nums){
-        if(n==1) return nums[0];
-        if(n<=0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=max(count(n-1,dp,nums), nums[n-1] + count(n-2,dp,nums));
+   int rob(vector<int>& nums) {
+        int n=nums.size();
+        int a=0;
+        int b=nums[0];
+        int c;
+        for(int i=2; i<=n; i++){
+            c=max(b,nums[i-1]+a);
+            a=b;
+            b=c;
+        }
+        return c;
     }
-    int rob(vector<int>& nums) {
+    int cob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n+1,0);
         dp[0]=0;
