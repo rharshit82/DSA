5720ae7ebcd4603438e83575a4621b402d37442c
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-24 01:10:48 +0530

    Time: 11 ms (69.92%), Space: 10.1 MB (49.56%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
+    int count(vector<vector<int>>& dp,vector<vector<int>>& grid,int n, int m){
+        if(m<0 or n<0) return 0;
+        if(m==0 and n==0) return grid[0][0];
+        if(dp[n][m]!=-1) return dp[n][m];
+        int top=INT_MAX,left=INT_MAX;
+        if(n-1>=0) top = grid[n][m]+ count(dp,grid,n-1,m);
+        if(m-1>=0) left = grid[n][m]+ count(dp,grid,n,m-1);
+        return dp[n][m]=min(top,left);
+    }
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        
-        for(int i=1; i<n; i++){
-            grid[i][0]+=grid[i-1][0];
-        }
-        for(int i=1; i<m; i++){
-            grid[0][i]+=grid[0][i-1];
-        }
-        for(int i=1;i<n; i++){
-            for(int j=1; j<m; j++){
-                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
-            }
-        }
-        return grid[n-1][m-1];
+        int n=grid.size(); int m=grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return count(dp,grid,n-1,m-1);
     }
 };
