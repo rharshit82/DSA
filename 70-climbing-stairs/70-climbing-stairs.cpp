943b6cd8a50d2fe53aa4a9b10716367de273ba5a
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-23 10:55:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.72%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
+    
+    int count(int n){
+        if(n==1 or n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=count(n-1) + count(n-2);
+    }
+    int dp[100000];
     int climbStairs(int n) {
-        if(n==0 or n==1 or n==2) return n;
-        int dp[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3; i<=n; i++){
-            dp[i]=dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        memset(dp,-1,sizeof(dp));
+        return count(n);
     }
 };
