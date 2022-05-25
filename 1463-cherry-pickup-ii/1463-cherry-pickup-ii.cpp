15f9a41d47658d0bd81b5fdc8f77004b47339e3c
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-25 22:15:33 +0530

    Time: 192 ms (17.11%), Space: 14.3 MB (48.30%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -5,29 +5,24 @@ public:
         int m=grid[0].size();
         if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
     return -1e9;
-
-  if (i == n - 1) {
-    if (j1 == j2)
-      return grid[i][j1];
-    else
-      return grid[i][j1] + grid[i][j2];
-  }
-
-  if (dp[i][j1][j2] != -1)
-    return dp[i][j1][j2];
-
-  int maxi = INT_MIN;
-  for (int di = -1; di <= 1; di++) {
-    for (int dj = -1; dj <= 1; dj++) {
-      int ans;
-      if (j1 == j2)
-        ans = grid[i][j1] + solve(dp, i + 1, j1 + di, j2 + dj,  grid );
-      else
-        ans = grid[i][j1] + grid[i][j2] + solve(dp, i + 1, j1 + di, j2 + dj, grid );
-      maxi = max(maxi, ans);
-    }
-  }
-  return dp[i][j1][j2] = maxi;
+        if(i==n-1) {
+            if(j1==j2) return grid[i][j1];
+            return grid[i][j1]+grid[i][j2];
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int mx = INT_MIN;
+        for (int di = -1; di <= 1; di++) {
+            int x;
+            for (int dj = -1; dj <= 1; dj++) {
+                if(j1==j2)
+                    x = grid[i][j1]+ solve(dp, i+1, j1+di, j2+ dj, grid);
+                else
+                    x = grid[i][j1]+ grid[i][j2]+ solve(dp, i+1, j1+di, j2+ dj, grid);
+                mx=max(x,mx);
+            }
+            
+        }
+        return dp[i][j1][j2]=mx;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
