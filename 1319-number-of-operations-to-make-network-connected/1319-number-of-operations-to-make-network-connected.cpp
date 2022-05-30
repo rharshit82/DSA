20fcca64102876db68cb6a8afaa961cb9ac85177
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-05-30 17:48:18 +0530

    Time: 208 ms (32.93%), Space: 37.3 MB (96.04%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<bool> visited;
-    void dfs(int i, vector<vector<int>> &graph){
-        visited[i]=true;
-        for(auto edge: graph[i]){
-            if(!visited[edge]) 
-                dfs(edge,graph);
-        }
+    int find(int x){
+        return parent[x]==x? x: find(parent[x]);
     }
+    vector<int> parent;
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1) return -1;
-        visited.resize(n,false);
-        vector<vector<int>> graph(n);
-        for(int i=0;i<connections.size();i++){
-            graph[connections[i][0]].push_back(connections[i][1]);
-            graph[connections[i][1]].push_back(connections[i][0]);
-        }
-        int res=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,graph);
-                res++;
+        if(n-1>connections.size()) return -1;
+        parent.resize(n);
+        for(int i=0; i<n; i++) parent[i]=i;
+        
+        for(int i=0; i<connections.size(); i++){
+            int a=connections[i][0];
+            int b=connections[i][1];
+            int x=find(a);
+            int y=find(b);
+            if(x!=y){
+                parent[x]=y;
             }
         }
-        return res-1;
+        int conn=0;
+        for(int i=0; i<n; i++){
+            if(parent[i]==i) conn++;
+        }
+        return conn-1;
     }
 };
