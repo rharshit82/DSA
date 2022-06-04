cbc9eeb39b75df0ad49a487358a5bcb1273a2a7e
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-06-04 19:39:27 +0530

    Time: 5 ms (20.22%), Space: 7.7 MB (47.52%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-   int rob(vector<int>& nums) {
-        int n=nums.size();
-        int a=0;
-        int b=nums[0];
-        int c;
-        for(int i=2; i<=n; i++){
-            c=max(b,nums[i-1]+a);
-            a=b;
-            b=c;
-        }
-        return c;
+    
+    int help(int i, vector<int>&dp,vector<int>& nums){
+        if(i>=nums.size()) return 0;
+        if(i==nums.size()-1) return nums[i];
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = max(nums[i]+ help(i+2,dp,nums), help(i+1,dp,nums));
     }
-    int cob(vector<int>& nums) {
+    int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=2; i<=n; i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
-        }
-        return dp[n];
+        vector<int> dp(n+1,-1);
+        return help(0,dp,nums);
     }
 };
