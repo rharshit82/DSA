b51329f4e559d2ff36ea3def525f506759074638
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-06-25 12:32:58 +0530

    Time: 16 ms (61.75%), Space: 7 MB (86.04%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -11,9 +11,11 @@ public:
         map<char,int> mp;
         int res=0;
         int l =0;
+        int mxf=0;
         for(int r=0; r<s.size();r++){
             mp[s[r]]++;
-            while((r-l+1 - hashmax(mp))>k){
+            mxf= max(mxf, mp[s[r]]);
+            while((r-l+1 - mxf)>k){
                 mp[s[l]]--;
                 l++;
             }
