489762e21955cfb9942590ae044d569d111aa484
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-06-29 15:18:31 +0530

    Time: 8 ms (58.58%), Space: 7.9 MB (47.45%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int findOne(int n){
-        int res=0;
-        int mask =1;
-        for(int j=0; j<31; j++){
-            if((mask<<j)&n) res++;
-        }
-        return res;
-    }
     vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i=0; i<=n; i++) res.push_back(findOne(i));
+        vector<int> res(n+1);
+        res[0]=0;
+        for(int i=1; i<=n; i++){
+            res[i] = res[i/2] + i%2;
+        }
         return res;
     }
 };
