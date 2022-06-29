b85e4b2c23e31099c889415158ceb6cc0210d1ab
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-06-29 16:35:01 +0530

    Time: 342 ms (6.95%), Space: 49.4 MB (11.36%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        
-        vector<int> dp(amount+1,1e8);
-        dp[0]=0;
-        for(int i=1; i<=amount; i++){
-            int mn = 1e8;
-            for(int j=1; j<=n; j++){
-                if(i-coins[j-1]>=0)  mn= min(mn,1+ dp[i-coins[j-1]]);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1,(int)1e8));
+        for(int i=0; i<=n; i++) dp[i][0]=0;
+        // for(int i=0; i<=amount; i++) dp[0][i]=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                if(j-coins[i-1]>=0){
+                    dp[i][j] = min(1+ dp[i][j-coins[i-1]],dp[i-1][j]);
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
-            dp[i]=mn;
         }
-        return (dp[amount]>=1e8? -1: dp[amount]);
+        return (dp[n][amount] == (int)1e8)? -1: dp[n][amount];
     }
 };
