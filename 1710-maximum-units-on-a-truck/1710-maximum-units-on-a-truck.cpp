0037829f280c8c82083982ac69a1dadffda90622
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-01 11:09:52 +0530

    Time: 79 ms (47.44%), Space: 16.3 MB (50.56%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    static bool mycomp(vector<int>& a, vector<int>& b){
-        return a[1]<b[1];
-    }
+   
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         int n=boxTypes.size();
-        sort(boxTypes.rbegin(),boxTypes.rend(), mycomp);
-        int res=0;
-        for(int i=0; i<n and truckSize>0; i++){
-            if(truckSize>=boxTypes[i][0]){
-                res+=boxTypes[i][1]*boxTypes[i][0];
-                truckSize-=boxTypes[i][0];
+        vector<int> bucket(1001, -1);
+        for(int i=0; i<n; i++){
+            int boxes = boxTypes[i][0];
+            int units = boxTypes[i][1];
+            
+            if(bucket[units]==-1){
+                bucket[units] = boxes;
             } else{
-                res+=(truckSize) * boxTypes[i][1];
+                bucket[units] += boxes;
+            }
+        }
+        int res=0;
+        for(int i=1000; i>=0 and truckSize>0; i--){
+            if(bucket[i]==-1) continue;
+            if(bucket[i]<=truckSize){
+                res+=bucket[i]*i;
+                truckSize-=bucket[i];
+            } else{
+                res+=truckSize*i;
                 break;
             }
         }
