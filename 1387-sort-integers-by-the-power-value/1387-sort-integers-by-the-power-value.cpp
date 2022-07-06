42ce05067396914a0d5cacb75fe9d573ec279843
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-06 10:09:40 +0530

    Time: 58 ms (61.51%), Space: 8.7 MB (87.91%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int x, vector<int>&dp){
+        if(x<=1) return 0;
+        // if(dp[x]!=-1) return dp[x];
+        if(x%2==0){
+            return 1 + solve(x/2,dp);
+        } else{
+            return 1+ solve(3*x+1,dp);
+        }
+        return 0;
+    }
+    static bool sortbysec(pair<int,int> a, pair<int,int> b){
+        if(a.second==b.second){
+            return a.first<b.first;
+        }
+        return a.second<b.second;
+    }
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int,int>> arr(hi-lo+1);
+        vector<int> dp(hi+1,-1);
+        int var=0;
+        for(int i=lo; i<=hi; i++){
+            arr[var++] = {i,solve(i,dp)};
+        }
+        
+        sort(arr.begin(),arr.end(),sortbysec);
+       
+        return arr[k-1].first;
+    }
+};
