4212eb2df01cc3a68420040b78ad8a1692460cca
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-06 20:42:53 +0530

    Time: 182 ms (36.61%), Space: 23.7 MB (26.82%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -6,20 +6,20 @@ public:
         int n=nums.size();
         for(int i=0; i<n; i++){
             int target = -nums[i];
-            int low = i+1;
-            int high = n-1;
-            while(low<high){
-                int sum = nums[low] + nums[high];
-                if(sum>target) high--;
-                else if(sum<target) low++;
-                else{
+            int j=i+1, k=n-1;
+            while(j<k){
+                if((nums[j]+nums[k])<target){
+                    j++;
+                } else if((nums[j]+nums[k])>target){
+                    k--;
+                } else{
                     vector<int> temp;
                     temp.push_back(nums[i]);
-                    temp.push_back(nums[low]);
-                    temp.push_back(nums[high]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
                     res.push_back(temp);
-                    while(low<high and nums[low]==temp[1]) low++;
-                    while(low<high and nums[high]==temp[2]) high--;
+                    while(j<n and nums[j]==temp[1]) j++;
+                    while(k>=0 and nums[k]==temp[2]) k--;
                 }
             }
             while(i+1<n and nums[i+1]==nums[i]) i++;
