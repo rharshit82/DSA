a11649ad3469365215db012562c19936beec9235
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-07 13:07:23 +0530

    Time: 8 ms (51.77%), Space: 6.9 MB (57.66%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    bool solve(string s1, string s2, string s3,int i, int j, int k,vector<vector<int>>&dp){
-        if(i==s1.size() and j==s2.size() and k==s3.size()) return true;
-        if(k==s3.size()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int a=false,b=false,c=false;
-        if(i<s1.size() and s1[i]==s3[k]){
-            a= solve(s1,s2,s3,i+1,j,k+1,dp);
-        }
-        if(j<s2.size() and s2[j]==s3[k]){
-            b= solve(s1,s2,s3,i,j+1,k+1,dp);
-        }
-        return dp[i][j]= a or b;
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        int n1= s1.size();
-        int n2= s2.size();
-        int n=s3.size();
-        vector<vector<int>> dp(n1+1, vector<int> (n2+1,-1));
-        return solve(s1,s2,s3,0,0,0,dp);
+        int n1=s1.size();
+        int n2=s2.size();
+        int n3=s3.size();
+        if(n1+n2!=n3) return false;
+        queue<pair<int,int>> q;
+        vector<vector<bool>> vis(n1+1, vector<bool>(n2+1,false));
+        q.push({0,0});
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            if(i==n1 and j==n2) return true;
+            if(vis[i][j]) continue;
+            if(i<n1 and s1[i]==s3[i+j]){
+                q.push({i+1,j});
+            }
+            if(j<n2 and s2[j]==s3[i+j]){
+                q.push({i,j+1});
+            }
+            vis[i][j]=true;
+        }
+        return false;
     }
 };
