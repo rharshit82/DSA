b92672d9cf8d253a93a28befd9fe47998f3a2f9a
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-07 20:17:59 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.43%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,13 +4,13 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         for(int i=0; i<n; i++){
-            for(int j=i; j<m; j++){
+            for(int j=i; j<n; j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0; i<n; i++){
-            for(int j=0; j<m/2; j++){
-                swap(matrix[i][j],matrix[i][n-j-1]);
+            for(int j=0; j<m/2 ;j++){
+                swap(matrix[i][j],matrix[i][m-j-1]);
             }
         }
     }
