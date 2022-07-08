efc404f21ddeda0473781712b151f7bd443bfb14
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-09 01:09:04 +0530

    Time: 117 ms (93.69%), Space: 61.3 MB (61.17%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int hare=0,tortoise=0;
+        int hare = 0;
+        int tortoise = 0;
+        int n=nums.size();
         do{
-            hare=nums[nums[hare]];
-            tortoise=nums[tortoise];
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
         }while(hare!=tortoise);
-        tortoise=0;
-        while(hare!=tortoise){
-            tortoise=nums[tortoise];
-            hare=nums[hare];
+        
+         tortoise=0;
+        while(tortoise!=hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        return hare;
+        return tortoise;
     }
 };
