38af2a5c68bf5a34a62ec017b0c71abaa8a43aea
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-11 14:05:35 +0530

    Time: 29 ms (50.90%), Space: 13.9 MB (23.88%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int> mp;
-        for(int i=0; i<n;i++){
+        map<int,int> mp;
+        
+        for(int i=0; i<n; i++){
             mp[nums[i]]++;
         }
+        
         int res=0;
         for(auto ele: mp){
             if(k==0){
                 if(ele.second>1) res++;
             }
-            else if(mp.find(ele.first+k)!=mp.end()) res++;
+            else if(mp.find(ele.first+k)!=mp.end()){
+                res++;
+            }
         }
         return res;
     }
