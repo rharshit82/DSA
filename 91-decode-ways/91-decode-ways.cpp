27e6a4c96aeb1b3cb016d9c49a362abc31bc0a64
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-12 14:50:14 +0530

    Time: 10 ms (11.20%), Space: 7.5 MB (15.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    
-    int solve(string s, int i,vector<int> &dp){
+    int ways(int i, string s, vector<int> &dp){
         int n=s.size();
         if(i>=n) return 1;
         if(dp[i]!=-1) return dp[i];
         int res=0;
-        int no = s[i]-'0';
+        int no = (s[i]-'0');
         int no2=0;
         if(i+1<n){
-             no2= no*10 + (s[i+1]-'0');
+            no2= 10*no + (s[i+1]-'0');
         }
-        if(no>0) res+=solve(s,i+1,dp);
-        if(no>0 and no2>0 and no2<=26) res+=solve(s,i+2,dp);
+        if(no>=1 and no<=26){
+            res+=ways(i+1, s, dp);
+            if(no2>=1 and no2<=26){
+                res+= ways(i+2, s, dp);
+            }
+        } 
         return dp[i]=res;
     }
     int numDecodings(string s) {
         int n=s.size();
-        vector<int> dp(n+1,-1);
-        return solve(s,0,dp);
+        vector<int> dp(n+1, -1);
+        return ways(0,s,dp);
     }
 };
