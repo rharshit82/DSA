f562ef8c972bd4fb78dc15dd68480fcd78662b9c
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-19 19:14:37 +0530

    Time: 532 ms (42.48%), Space: 141.5 MB (56.32%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -9,6 +9,11 @@ public:
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
                 int diff = nums[i]- nums[j]+500;
+            // We simply put:
+            // int diff = nums[i] - nums[j] +500;
+            // this will ensure the index: diff is between 0 and 1000, 
+                // since the maximum can be 1000,               
+                // the size of the one dimensional dp should be 1001
                 dp[i][diff] = (dp[j][diff]> 0)? dp[j][diff]+1: 2;
                 res = max(res, dp[i][diff]);
             }
