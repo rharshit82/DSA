29a70ef45b91671aa9497f1f63dc787613b5382b
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-25 21:29:11 +0530

    Time: 15 ms (52.42%), Space: 17.4 MB (51.60%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-    
-     if(!root) return 0;
-        
-        //similar to level order
-        queue<pair<TreeNode *, unsigned long long int>> q;
-        int maxwidth =0;
-        q.push(make_pair(root, 0));
-        
+        if(!root)
+            return 0;
+        unsigned long long ans = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root,0});
         while(!q.empty()){
-            
-            
-            unsigned long long int left_index = q.front().second, right_index = 0;
-            unsigned long long int sz = q.size();
-            
-            
-            for(unsigned long long int i=0; i<sz; i++){
-                
-                pair<TreeNode *, unsigned long long int> p = q.front();
+            int size = q.size();
+            unsigned long long mmin = q.front().second;    //to make the id starting from zero
+            unsigned long long first,last;
+            for(int i=0; i<size; i++){
+                unsigned long long cur_id = q.front().second-mmin;
+                TreeNode* node = q.front().first;
                 q.pop();
-                TreeNode *temp = p.first;
-                right_index = p.second;
-                
-                if(temp->left)
-                    q.push(make_pair(temp->left, 2*right_index + 1));
-                if(temp->right)
-                    q.push(make_pair(temp->right, 2*right_index + 2));
-                
+                if(i==0) first = cur_id;
+                if(i==size-1) last = cur_id;
+                if(node->left)
+                    q.push({node->left, cur_id*2+1});
+                if(node->right)
+                    q.push({node->right, cur_id*2+2});
             }
-            
-            maxwidth = max(maxwidth, int(right_index - left_index + 1));
+            ans = max(ans, last-first+1);
         }
-        
-        return maxwidth;
-        
+        return ans;
     }
 };
