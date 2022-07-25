776a44cf3c41661eacdac5d231c3e815a1d1bbb8
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-07-25 21:35:51 +0530

    Time: 15 ms (52.42%), Space: 17.4 MB (51.60%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,28 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)
-            return 0;
-        unsigned long long ans = 0;
+        if(!root) return 0;
         queue<pair<TreeNode*, int>> q;
+        unsigned long long res=0;
         q.push({root,0});
         while(!q.empty()){
-            int size = q.size();
-            unsigned long long mmin = q.front().second;    //to make the id starting from zero
-            unsigned long long first,last;
-            for(int i=0; i<size; i++){
-                unsigned long long cur_id = q.front().second-mmin;
-                TreeNode* node = q.front().first;
+            int sz = q.size();
+            unsigned long long mmin = q.front().second;
+            unsigned long long left, right;
+            for(int i=0; i<sz; i++){
+                TreeNode* curr = q.front().first;
+                unsigned long long vl = q.front().second - mmin;
                 q.pop();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node->left)
-                    q.push({node->left, cur_id*2+1});
-                if(node->right)
-                    q.push({node->right, cur_id*2+2});
+                if(i==0) left = vl;
+                if(i==sz-1) right = vl;
+                if(curr->left){
+                    q.push({curr->left,2*vl+1});
+                }
+                if(curr->right){
+                    q.push({curr->right,2*vl+2});
+                }
             }
-            ans = max(ans, last-first+1);
+            res = max(res, right-left+1);
         }
-        return ans;
+        return res;
     }
 };
