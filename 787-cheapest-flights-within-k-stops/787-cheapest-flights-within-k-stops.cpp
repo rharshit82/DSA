ba2baeb67932fec2e080b1fca70a6c05ac2e8021
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-08-06 11:13:51 +0530

    Time: 343 ms (11.82%), Space: 49.6 MB (9.70%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-   int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>> dp(n, vector<int> (n, 1e9));
-        int len = flights.size();
-        int cycles = k;
-        if(k<n-1) cycles++;
-        dp[0][src] = 0;        
-        for(int i=1; i<=cycles; i++){
-            for(int j=0; j<len; j++){
-                int from = flights[j][0];
-                int to = flights[j][1];
-                int weight= flights[j][2];
-                dp[i][src] = dp[i-1][src]; //Every cycle src weight should be 0
-                dp[i][to] = min(dp[i-1][to],min(dp[i][to], dp[i-1][from]+weight)); // From previous cycle To node, From previous Cycle From Node + weight, previously calculated value
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, INT_MAX);
+        dist[src]=0;
+        for(int i=0; i<=k; i++){
+            vector<int> temp(dist);
+            for(auto flight: flights){
+                int from = flight[0];
+                int to = flight[1];
+                int weight = flight[2];
+                if(dist[from]!=INT_MAX){
+                    temp[to] = min(temp[to],dist[from]+weight);
+                }
             }
+            dist = temp;
         }
-        return (dp[cycles][dst]==1e9? -1: dp[cycles][dst]);
+        return dist[dst] == INT_MAX? -1: dist[dst];
     }
 };
