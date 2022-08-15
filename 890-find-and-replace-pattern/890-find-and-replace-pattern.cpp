6b5a64a5097c1a973ed81dd83de4d99aacfee992
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-08-15 11:13:13 +0530

    Time: 13 ms (11.63%), Space: 8.3 MB (69.63%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string& pattern) {        
-	vector<string> ans;
-	string p = normalise(pattern);
-	for(auto& w : words) 
-		if(p == normalise(w)) ans.push_back(w);
-	return ans;
-}
-string normalise(string w) {
-	unordered_map<char, char> mp;
-	char c = 'a';
-	for(auto ch : w) 
-		mp[ch] = c++;
-	for(auto& ch : w) ch = mp[ch];
-	return w;
-}
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        string normal = normalize(pattern);
+        vector<string> res;
+        for(auto word: words){
+            if(normal == normalize(word)){
+                res.push_back(word);
+            }
+        }
+        return res;
+    }
+    string normalize(string word){
+        unordered_map<char,char> mp;
+        char init = 'a';
+        for(auto ch: word){
+            mp[ch] = init++;
+        }
+        for(auto &ch: word){
+            ch = mp[ch];
+        }
+        return word;
+    }
 };
