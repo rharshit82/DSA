02ef1dfe663e85bca8fa7deef3ecffb0ec01ebc1
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2022-09-26 22:28:33 +0530

    Time: 49 ms (72.91%), Space: 23.9 MB (63.81%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    // Loop once, we can get the Next Greater Number of a normal array.
-    // Loop twice, we can get the Next Greater Number of a circular array
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> res(n,-1);
+        int n = nums.size();
+        vector<int> res(n);
         stack<int> st;
-        for(int i=2*n-1; i>=0; i--){
-            int num = nums[i%n];
-            while(!st.empty() and st.top()<= num) st.pop();
-            if(!st.empty()) res[i%n]=st.top();
-            st.push(num);
+        for(int i= 2*n-1; i>=0; i--){
+            while(!st.empty() and st.top()<=nums[i%n]) st.pop();
+            if(st.empty()) res[i%n] = -1;
+            else res[i%n] = st.top();
+            st.push(nums[i%n]);
         }
         return res;
     }
