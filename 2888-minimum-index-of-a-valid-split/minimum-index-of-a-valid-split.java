bb962b83da04cd5bd1a1d4be31ee9003ae060aa4
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2025-04-12 04:59:31 +0530

    Time: 11 ms (85.69%) | Memory: 56.7 MB (74.02%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -2,15 +2,18 @@ class Solution {
     int[] majorityElement(List<Integer> nums) {
         int mj = -1;
         int count = 0;
-        for (int ele : nums) {
+        for(int ele : nums){
             if (count == 0) {
-                mj = ele;
-                count = 1;
-            } else if (ele == mj) {
+    mj = ele;
+    count = 1;
+}
+
+            else if(ele == mj){
                 count++;
             } else {
                 count--;
             }
+            
         }
         int freq = 0;
         for (int num : nums) {
@@ -18,27 +21,26 @@ class Solution {
         }
         return new int[]{mj, freq};
     }
-
     public int minimumIndex(List<Integer> nums) {
         int[] mjEleRes = majorityElement(nums);
         int mjEle = mjEleRes[0];
-        int totalCount = mjEleRes[1];
-
+        int count = mjEleRes[1];
         int currCount = 0;
         int n = nums.size();
-
-        for (int i = 0; i < n - 1; i++) {
-            if (nums.get(i) == mjEle) {
+        for(int i= 0; i<n - 1; i++){
+            int ele = nums.get(i);
+            if(ele == mjEle){
                 currCount++;
             }
 
-            // Check dominant in left: currCount > (i + 1) / 2
-            // Check dominant in right: (totalCount - currCount) > (n - i - 1) / 2
-            if (currCount * 2 > (i + 1) && (totalCount - currCount) * 2 > (n - i - 1)) {
-                return i;
-            }
+            int left = currCount;
+            int right = n - left;
+           if (currCount * 2 > (i + 1) && (count - currCount) * 2 > (n - i - 1)) {
+    return i;
+}
         }
-
+        
         return -1;
+
     }
 }
