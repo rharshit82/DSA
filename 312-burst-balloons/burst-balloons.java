df07660c8dcfefd4f9b8063638f261f891c20742
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2025-08-15 18:39:56 +0530

    Time: 91 ms (58.30%) | Memory: 43.2 MB (10.07%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,22 +1,20 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        int n = nums.length;
-        int arr[] = new int[n+2];
-        arr[0] = 1;
-        for(int i=1; i<=n; i++) arr[i] = nums[i-1];
-        arr[n+1] = 1;
-
-        int[][] dp = new int[n+2][n+2];
-        for(int len = 2; len<= n+1; len++){
-            for(int i = 0; i+len <=n+1; i++){
-                int j = i + len;
-                dp[i][j] = Integer.MIN_VALUE;
-                for(int k = i+1; k<j; k++){
-                    dp[i][j] = Math.max(dp[i][j], arr[i]*arr[k]*arr[j] + dp[k][j] + dp[i][k]);
+        List<Integer> paddedNums = new ArrayList<>();
+        paddedNums.add(1);
+        Arrays.stream(nums).forEach(paddedNums::add);
+        paddedNums.add(1);
+        int n = paddedNums.size();
+        int[][] dp = new int[n][n];
+        for(int length = 2; length<=n; length++){
+            for(int l=0; l + length <= n; l++){
+                int r = l + length - 1;
+                for(int k=l+1; k<r; k++){
+                    dp[l][r] = Math.max(dp[l][r], dp[l][k] + paddedNums.get(k) * paddedNums.get(l) * paddedNums.get(r) + dp[k][r]);
                 }
             }
         }
-        return dp[0][n+1];
-
+        return dp[0][n-1];
+            
     }
 }
